671a01ee99c24342a727801bf97c78076062fdfb
Olex Taraniuk <taran76@gmail.com>
2026-01-22 22:08:24 +0200

    refactor: remove calculator module and clean up main.py by deleting unused code and comments

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 
 # main.py
-import sys
 
 age: int = 27
 name: str = "Sasha"
@@ -8,21 +7,3 @@ height: float = 1.75
 is_worker: bool = True
 
 print(f"Age: {age}, Name: {name}, Height: {height}, Is Worker: {is_worker}")
-
-
-# def main():
-#     print(f"Hello from Python {sys.version}")
-#     print("Project initialized successfully!")
-
-
-# if __name__ == "__main__":
-#     main()
-
-#     my_list = [1, 2, 3, 4, 5]
-
-#     print(calculator(10, 2, "+"))
-#     print(calculator(10, 2, "-"))
-#     print(calculator(10, 2, "*"))
-#     print(calculator(10, 2, "/"))
-#     print(calculator(10, 2, "%"))
-#     print(calculator(10, 2, "sqrt"))
