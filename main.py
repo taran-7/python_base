55cac6cee2797b6f592e212315c08b395c488d75
Olex Taraniuk <taran76@gmail.com>
2026-01-22 22:07:09 +0200

    refactor: reorganize main.py by removing calculator imports and cleaning up commented code

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
+
+# main.py
 import sys
 
-from calculator import calculator
+age: int = 27
+name: str = "Sasha"
+height: float = 1.75
+is_worker: bool = True
+
+print(f"Age: {age}, Name: {name}, Height: {height}, Is Worker: {is_worker}")
 
 
-def main():
-    print(f"Hello from Python {sys.version}")
-    print("Project initialized successfully!")
+# def main():
+#     print(f"Hello from Python {sys.version}")
+#     print("Project initialized successfully!")
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
 
-    my_list = [1, 2, 3, 4, 5]
+#     my_list = [1, 2, 3, 4, 5]
 
-    print(calculator(10, 2, "+"))
-    print(calculator(10, 2, "-"))
-    print(calculator(10, 2, "*"))
-    print(calculator(10, 2, "/"))
-    print(calculator(10, 2, "%"))
-    print(calculator(10, 2, "sqrt"))
+#     print(calculator(10, 2, "+"))
+#     print(calculator(10, 2, "-"))
+#     print(calculator(10, 2, "*"))
+#     print(calculator(10, 2, "/"))
+#     print(calculator(10, 2, "%"))
+#     print(calculator(10, 2, "sqrt"))
