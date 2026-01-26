b354ec851b35055e2d7de3aa78c5d52b2f202773
Olex Taraniuk <taran76@gmail.com>
2026-01-26 23:02:27 +0200

    refactor: clean up dict.py by commenting out unused user dictionary and related code

--- dict.py
+++ dict.py
@@ -1,20 +1,32 @@
 import sys
 
-# Створення словника
-user = {
-    "name": "Олексій",
-    "age": 25,
-    "city": "Київ"
+# # Створення словника
+# user = {
+#     "name": "Олексій",
+#     "age": 25,
+#     "city": "Київ"
+# }
+
+city = {
+    "name": "Kyiv",
+    "population": 3_500_000,
+    "electrisity": "230V",
+    "regions": ["Shevchenkivskyi", "Pecherskyi", "Podil", "Voskresenka"]
 }
 
 # Доступ до значення за ключем
-print(user["name"])  # Виведе: Олексій
+# print(user["name"])  # Виведе: Олексій
+print(city["regions"])
 
-# Додавання нової пари
-user["job"] = "Developer"
+# # Додавання нової пари
+# user["job"] = "Developer"
+city["big"] = True
 
-# Зміна значення
-user["age"] = 26
 
-print(user)
-# Виведе: {'name': 'Олексій', 'age': 26, 'city': 'Київ', 'job': 'Developer'}
+# # Зміна значення
+# user["age"] = 26
+city["population"] = 3_700_000
+
+# print(user)
+# # Виведе: {'name': 'Олексій', 'age': 26, 'city': 'Київ', 'job': 'Developer'}
+print(f"Словник: {city}")
